744950d0ed9c96e2766a3437575c5a8a811e5f29
Alex Curran <alexcurran.va@gmail.com>
2021-05-07 11:06:37 -0500

    Starting files

---
+++ src/components/Layout/MainHeader.js
@@ -0,0 +1,28 @@
+import { useDispatch } from "react-redux";
+import { uiActions } from "../../store/ui-slice"
+
+import CartButton from '../Cart/CartButton';
+import classes from './MainHeader.module.css';
+
+const MainHeader = (props) => {
+  const dispatch = useDispatch();
+
+  function toggleCartHandler() {
+    dispatch(uiActions.toggle());
+  }
+
+  return (
+    <header className={classes.header} onClick={toggleCartHandler}>
+      <h1>ReduxCart</h1>
+      <nav>
+        <ul>
+          <li>
+            <CartButton />
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainHeader;
